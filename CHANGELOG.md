d01e52a637bb0bdaa6e5c58fcd85f7bb2e88a5c8
Jonathan Hartman <j@p4nt5.com>
2015-07-06 22:49:46 -0700

    Initial commit; a freshly generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Parallels Cookbook CHANGELOG
+============================
+
+v0.0.1 (2015-07-06)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'parallels'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures parallels'
+long_description 'Installs/Configures parallels'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
